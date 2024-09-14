897c1a36eeb5ef5e2fb84095b4a6345043e07d34
FAHAD KABIR <hmdipto7@gmail.com>
2024-09-14 12:06:40 -0230

    sing out

--- Simple-firebase/src/components/Login/Login.jsx
+++ Simple-firebase/src/components/Login/Login.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import app from '../../../firebase/firebase.innit';
-
+import React, { useState } from "react";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../../firebase/firebase.innit";
 
 const Login = () => {
-    const auth = getAuth(app);
-    console.log(app)
-    const provider = new GoogleAuthProvider;
-    const handleGoogleLogin = ()=>{
-        signInWithPopup(auth, provider)
-        .then( result=>{
-            const user = result.user
-            console.log(user)
-        })
-        .catch(error =>{
-            console.log('error', error.message )
-        })
-    }
-    return (
-        <div>
-            <button onClick={handleGoogleLogin}>Google</button>
-            
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const [user, setUser] = useState(null);
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        setUser(loggedInUser);
+        console.log(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleSignout = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="">
+{ 
+  user?
+  <button onClick={handleSignout}> Logout</button> :
+        <button onClick={handleGoogleLogin}>Google</button> 
+     }
+      {user && (
+        <div className="">
+          <h2>User: {user.displayName}</h2>
+          <h2>Email: {user.email}</h2>
+          <img src={user.photoURL} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
